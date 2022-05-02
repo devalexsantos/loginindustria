4bfff5898b5d1b1027f72d3ddea077031488a455
Alex Santos <devalexsantos@gmail.com>
2022-05-02 08:28:27 -0300

    customizando menus

--- components/Topbar.js
+++ components/Topbar.js
@@ -1,81 +1,78 @@
-import { Link } from '@chakra-ui/react';
-import Image from 'next/image';
-import { useState } from 'react';
+import { Link } from "@chakra-ui/react";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Topbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
-    const [navbarOpen, setNavbarOpen] = useState(false);
-
-    return (
-        <div className="container flex items-center mx-auto justify-between p-3 tracking-wide">
-            <nav className="container relative flex flex-wrap items-center justify-between">
-                <div className="container mx-auto flex flex-wrap items-center justify-between">
-                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                        <a
-                            className="text-sm leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-                            href="#pablo"
-                        >
-                            <Image
+  return (
+    <div className="container flex items-center mx-auto justify-between p-3 tracking-wide">
+      <nav className="container relative flex flex-wrap items-center justify-between">
+        <div className="container mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-sm leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+              href="#pablo"
+            >
+              <Image
                 src="/logo_top.jpg"
                 alt="Picture of the author"
-                width={'342px'}
-                height={'68px'}
-            />
-                        </a>
-                        <button
-                            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                            type="button"
-                            onClick={() => setNavbarOpen(!navbarOpen)}
-                        >
-                            <i className="fas fa-bars"></i>
-                        </button>
-                    </div>
-                    <div
-                        className={
-                            "lg:flex flex-grow items-center" +
-                            (navbarOpen ? " flex" : " hidden")
-                        }
-                        id="example-navbar-danger"
-                    >
-                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto m-auto text-center text-sm">
-                            <li className="nav-item">
-                                <Link href="/" style={{ textDecoration: 'none' }}>
-                                <div
-                                    className="px-3 py-2 flex items-center uppercase hover:bg-primary hover:text-white rounded mx-2"
-                                >
-                                    INÍCIO
-                                </div>
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link href="/computadores" style={{ textDecoration: 'none' }}>
-                                <div
-                                    className="px-3 py-2 flex items-center uppercase hover:bg-primary hover:text-white hover:no-underline rounded mx-2"
-                                >
-                                    COMPUTADORES
-                                </div>
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <div
-                                    className="px-3 py-2 flex items-center uppercase hover:bg-primary hover:text-white rounded mx-2"
-                                    href="#pablo"
-                                >
-                                    CENTRAL DE DOWNLOADS
-                                </div>
-                            </li>
-                            <li className="nav-item">
-                                <div
-                                    className="px-3 py-2 flex items-center uppercase hover:bg-primary hover:text-white rounded border mx-2"
-                                    href="#pablo"
-                                >
-                                    SUPORTE
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+                width={"342px"}
+                height={"68px"}
+              />
+            </a>
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto m-auto text-center text-sm">
+              <li className="nav-item">
+                <Link href="/" style={{ textDecoration: "none" }}>
+                  <div className="px-3 py-2 flex items-center uppercase hover:bg-primary hover:text-white rounded mx-2">
+                    INÍCIO
+                  </div>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/computadores" style={{ textDecoration: "none" }}>
+                  <div className="px-3 py-2 flex items-center uppercase hover:bg-primary hover:text-white hover:no-underline rounded mx-2">
+                    COMPUTADORES
+                  </div>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  href="https://www.centraldedownload.com.br/"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                >
+                  <div className="px-3 py-2 flex items-center uppercase hover:bg-primary hover:text-white rounded mx-2">
+                    CENTRAL DE DOWNLOADS
+                  </div>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/suporte" style={{ textDecoration: "none" }}>
+                  <div className="px-3 py-2 flex items-center uppercase hover:bg-primary hover:text-white rounded border mx-2">
+                    SUPORTE
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
--- pages/suporte.js
+++ pages/suporte.js
@@ -1,23 +1,90 @@
-import Head from 'next/head';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import React, { useState } from "react";
 
 export default function Suporte() {
+  const [route, setRoute] = useState("atendimento");
+
+  const handleMenu = (menu) => {
+    if (menu === "atendimento") setRoute("atendimento");
+    if (menu === "politica") setRoute("politica");
+    if (menu === "chamados") setRoute("chamados");
+    if (menu === "assistencias") setRoute("assistencias");
+  };
+
   return (
     <div>
       <Head>
         <title>Suporte - Login Indústria</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Setor Coorporativo da Login Informática"></meta>
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700;800&display=swap" rel="stylesheet"></link>
+        <meta
+          name="description"
+          content="Setor Coorporativo da Login Informática"
+        ></meta>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700;800&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
       <main className="bg-gray-100 font-body">
-        <Header/>
-
-        <Footer/>
+        <Header />
+        <div className="flex justify-center bg-white py-20">
+          <div className="flex flex-col items-center container">
+            <h3 className="text-4xl md:text-5xl font-bold text-primary text-center mb-8">
+              Suporte
+            </h3>
+            <div className="flex flex-wrap justify-center md:px-20 lg:px-30 items-center w-full">
+              <div className="md:w-1/4 sm:w-full text-center">
+                <ul>
+                  <li
+                    className={`cursor-pointer p-1 ${
+                      route === "atendimento" && "bg-primary rounded text-white"
+                    }`}
+                    onClick={() => handleMenu("atendimento")}
+                  >
+                    Canais de Atendimento
+                  </li>
+                  <li
+                    className={`cursor-pointer p-1 ${
+                      route === "chamados" && "bg-primary rounded text-white"
+                    }`}
+                    onClick={() => handleMenu("chamados")}
+                  >
+                    Abertura de Chamados
+                  </li>
+                  <li
+                    className={`cursor-pointer mt-1 p-1 ${
+                      route === "politica" && "bg-primary rounded text-white"
+                    }`}
+                    onClick={() => handleMenu("politica")}
+                  >
+                    Política de Garantia
+                  </li>
+                  <li
+                    className={`cursor-pointer mt-1 p-1 ${
+                      route === "assistencias" &&
+                      "bg-primary rounded text-white"
+                    }`}
+                    onClick={() => handleMenu("assistencias")}
+                  >
+                    Assistências Autorizadas
+                  </li>
+                </ul>
+              </div>
+              <div className="md:w-3/4  sm:w-full text-left">
+                {route === "atendimento" && <p>Atendimento</p>}
+                {route === "chamados" && <p>Abertura de Chamados</p>}
+                {route === "politica" && <p>Política de Garantia</p>}
+                {route === "assistencias" && <p>Assistências Autorizadas</p>}
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer />
       </main>
     </div>
-  )
+  );
 }
